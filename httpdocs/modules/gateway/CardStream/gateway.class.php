9b97ba7e409f3d494d0d402cc50f63d1fab294c9
Paul Lashbrook <snider@users.noreply.github.com>
2015-06-22 15:16:47 +0100

    Update gateway.class.php
    adds callback processing
    better payment error handling
    tested to work in v6

--- httpdocs/modules/gateway/CardStream/gateway.class.php
+++ httpdocs/modules/gateway/CardStream/gateway.class.php
@@ -53,6 +53,7 @@
 				'transactionUnique' => md5($this->_basket['cart_order_id'].time()),
 				'orderRef' => $this->_basket['cart_order_id'],
 				'redirectURL' => $GLOBALS['storeURL'].'/index.php?_g=rm&type=gateway&cmd=process&module=CardStream',
+				'callbackURL' => $GLOBALS['storeURL'].'/index.php?_g=rm&type=gateway&cmd=process&module=CardStream&callback=true',
 				'customerAddress' => $address,
 				'customerPostCode' => $this->_basket['billing_address']['postcode'],
 				'customerEmail' => $this->_basket['billing_address']['email'],
@@ -88,36 +89,61 @@
 			$cart_order_id		= $_POST['orderRef'];
 			$order_summary		= $order->getSummary($cart_order_id);
 
-			if (isset($_POST['signature'])) {
-				$check = $_POST;
-				unset($check['signature']);
-				ksort($check);
-				$build_query =  http_build_query($check, '', '&');
-				$build_query = preg_replace('/%0D%0A|%0A%0D|%0A|%0D/i', '%0A', $build_query);
-				$sig_check = ($_POST['signature'] == hash("SHA512", $build_query. $this->_module['merchant_passphrase']));
+			if((int)$order_summary['status'] != (int)Order::ORDER_PROCESS) {
+
+
+				if ( isset( $_POST['signature'] ) ) {
+					$check = $_POST;
+					unset( $check['signature'] );
+					ksort( $check );
+					$build_query = http_build_query( $check, '', '&' );
+					$build_query = preg_replace( '/%0D%0A|%0A%0D|%0A|%0D/i', '%0A', $build_query );
+					$sig_check   = ( $_POST['signature'] == hash( "SHA512", $build_query . $this->_module['merchant_passphrase'] ) );
+				} else {
+					$sig_check = true;
+				}
+
+				if ( $_POST['responseCode'] == '0' && $sig_check ) {
+					$order->orderStatus( Order::ORDER_PROCESS, $cart_order_id );
+					$order->paymentStatus( Order::PAYMENT_SUCCESS, $cart_order_id );
+				}elseif($_POST['responseCode'] == '5'){
+					$order->orderStatus(Order::ORDER_DECLINED, $cart_order_id);
+					$order->paymentStatus( Order::PAYMENT_DECLINE, $cart_order_id );
+				}elseif($_POST['responseCode'] != '0'){
+					$order->orderStatus(Order::ORDER_DECLINED, $cart_order_id);
+					$order->paymentStatus( Order::PAYMENT_FAILED, $cart_order_id );
+				}
+
+				$transData['notes']       = $sig_check == true ? 'response signature check verified' : 'response signature check failed';
+				$transData['gateway']     = 'CardStream';
+				$transData['order_id']    = $_POST['orderRef'];
+				$transData['trans_id']    = $_POST['xref'];
+				$transData['amount']      = ( $_POST['amountReceived'] > 0 ) ? ( $_POST['amountReceived'] / 100 ) : '';
+				$transData['status']      = $_POST['responseMessage'];
+				$transData['customer_id'] = $order_summary['customer_id'];
+				$transData['extra']       = '';
+				$order->logTransaction( $transData );
+			}
+
+
+
+			if(!isset($_GET['callback'])){
+				// ensure the module path is not in the url, had a bug with order emails having weird links
+				$url = explode('/modules/gateway/CardStream',$GLOBALS['storeURL']);
+
+				httpredir($url[0].'/index.php?_a=complete');
 			}else{
-				$sig_check = true;
+				$transData['notes']       =  'callback processed' ;
+				$transData['gateway']     = 'CardStream';
+				$transData['order_id']    = $_POST['orderRef'];
+				$transData['trans_id']    = $_POST['xref'];
+				$transData['amount']      = ( $_POST['amountReceived'] > 0 ) ? ( $_POST['amountReceived'] / 100 ) : '';
+				$transData['status']      = $_POST['responseMessage'];
+				$transData['customer_id'] = $order_summary['customer_id'];
+				$transData['extra']       = '';
+				$order->logTransaction( $transData );
 			}
 
-			if($_POST['responseCode'] == '0' && $sig_check){
-				$order->orderStatus(Order::ORDER_PROCESS, $cart_order_id);
-				$order->paymentStatus(Order::PAYMENT_SUCCESS, $cart_order_id);
-			}
-
-			$transData['notes']			= $sig_check == true ? 'response signature check verified' : 'response signature check failed';
-			$transData['gateway']		= 'CardStream';
-			$transData['order_id']		= $_POST['orderRef'];
-			$transData['trans_id']		= $_POST['xref'];
-			$transData['amount']		= ($_POST['amountReceived']>0) ? ($_POST['amountReceived']/100) : '';
-			$transData['status']		= $_POST['responseMessage'];
-			$transData['customer_id']	= $order_summary['customer_id'];
-			$transData['extra']			= '';
-			$order->logTransaction($transData);
-
-			// ensure the module path is not in the url, had a bug with order emails having weird links
-			$url = explode('/modules/gateway/CardStream',$GLOBALS['storeURL']);
-
-			httpredir($url[0].'/index.php?_a=complete');
 
 			return false;
 		}
